476979f24cbbb129626eccf74d9a66a6c2c7a24d
james.cyhuang <james.cyhuang@mic.com.tw>
2020-07-17 14:09:59 +0800

    Update picture

--- gifgenerator.py
+++ gifgenerator.py
@@ -1,8 +1,9 @@
 import imageio
 images = []
 
-filenames = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg']
+filenames = ['1', '2', '3', '4', '5', '6']
 
 for filename in filenames:
+    filename += ".jpg"
     images.append(imageio.imread(filename))
 imageio.mimsave('movie.gif', images, duration=1)
